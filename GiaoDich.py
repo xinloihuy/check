aaf33807d18de31f6931cae169c468243715138d
ChiTrungVo <vochitrung212@gmail.com>
2024-12-03 13:21:44 +0700

    GiaoDich.py

--- GiaoDich.py
+++ GiaoDich.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 class GiaoDich:
     _sotudong=0
-    def __init__(self,loai):
+    def __init__(self,loai,sotien,ghichu):
         GiaoDich._sotudong+=1
         self.magiaodich=self.TaoMaGD()
         self.LoaiGD=loai
-        self.sotien=float(input("Nhập số tiền:"))
+        self.sotien=sotien
         self.ngaygiaodich=datetime.now().strftime('%d-%m-%Y')
-        self.ghichu=input("Ghi chú( nếu không có ghi chú chỉ cần nhập /):")
+        self.ghichu=ghichu
     @classmethod
     def TaoMaGD(cls):
-        ngay=datetime.now().strftime("%Y%m%d")
+        ngay=datetime.now().strftime("%d%m%Y")
         return f"GD-{ngay}-{cls._sotudong:04d}"
     def Xuat(self):
         return (f"Mã giao dịch: {self.magiaodich}\n"
@@ -18,4 +18,3 @@ class GiaoDich:
                 f"Ngày giao dịch: {self.ngaygiaodich}\n"
                 f"Số tiền: {self.sotien}\n"
                 f"Ghi chú: {self.ghichu}\n")
-
